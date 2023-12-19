b6207e62f457ae9653d08bbb701bf13c851518b4
Linfindel <116459177+linfindel@users.noreply.github.com>
2023-12-19 20:35:00 +0000

    Epic intro

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ setTimeout(() => {
     document.getElementById("search-box").style.opacity = "1";
     document.getElementById("search-box").style.transform = "translateY(0)";
 
+    document.getElementById("navbar").style.opacity = "1";
+    document.getElementById("navbar").style.transform = "translateY(0)";
+
+    document.getElementById("hub-button").style.opacity = "1";
+    document.getElementById("hub-button").style.transform = "translateX(0)";
+
     setTimeout(() => {
         document.getElementById("search-box").style.transition = "0.25s ease";
+
+        document.getElementById("navbar").style.transition = "0.25s ease";
+
+        document.getElementById("hub-button").style.transition = "0.25s ease";
+
         document.body.style.overflow = "initial";
     }, 2500);
 }, 5000);
