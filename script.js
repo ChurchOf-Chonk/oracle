5932d545843ebe5f0e3fd384cf2b82dbd31d0b20
Linfindel <116459177+linfindel@users.noreply.github.com>
2023-12-19 20:31:03 +0000

    Intro improvements

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 setTimeout(() => {
     document.getElementById("dots").style.animation = "dots-final 2.5s ease alternate infinite both";
+    
+    document.getElementById("search-box").style.opacity = "1";
+    document.getElementById("search-box").style.transform = "translateY(0)";
+
+    setTimeout(() => {
+        document.getElementById("search-box").style.transition = "0.25s ease";
+        document.body.style.overflow = "initial";
+    }, 2500);
 }, 5000);
