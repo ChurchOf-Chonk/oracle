8b58a94d95c833b3d664e1df9593ca5eeb254f8d
Linfindel <116459177+linfindel@users.noreply.github.com>
2023-12-20 11:42:59 +0000

    Add some basic responses

--- script.js
+++ script.js
@@ -20,3 +20,157 @@ setTimeout(() => {
         document.body.style.overflow = "initial";
     }, 2500);
 }, 5000);
+
+function respond() {
+    document.getElementById("output").style.opacity = "0";
+
+    setTimeout(() => {
+        var input = document.getElementById("input").value;
+
+        var inputUnsanitised = input;
+        inputUnsanitised = swapPronouns(inputUnsanitised);
+
+        input = input.toLowerCase();
+        input = swapPronouns(input);
+
+        var output;
+
+        if (input.match(/^is (\w+) (a|an|the) (\w+)(\?)?$/i)) {
+            var [, subject, article, object] = input.match(/^is (\w+) (a|an|the) (\w+)(\?)?$/i);
+
+            var [, subjectCapitalised, article, objectCapitalised] = inputUnsanitised.match(/^is (\w+) (a|an|the) (\w+)(\?)?$/i)
+
+            if (subject != subjectCapitalised) {
+                subject = subjectCapitalised;
+            }
+
+            if (object != objectCapitalised) {
+                object = objectCapitalised;
+            }
+
+            const binary = yesNo();
+
+            var isOrNot;
+
+            if (binary == "Yes") {
+                isOrNot = "is";
+            }
+
+            else {
+                isOrNot = "is not";
+            }
+            
+            output = `${binary}, ${subject} ${isOrNot} ${article} ${object}`;
+        }
+
+        else if (input.match(/^is (\w+) (\w+)(\?)?$/i)) {
+            var [, subject, object] = input.match(/^is (\w+) (\w+)(\?)?$/i);
+            var [, subjectCapitalised, objectCapitalised] = inputUnsanitised.match(/^is (\w+) (\w+)(\?)?$/i);
+
+            if (subject != subjectCapitalised) {
+                subject = subjectCapitalised;
+            }
+
+            if (object != objectCapitalised) {
+                object = objectCapitalised;
+            }
+
+            const binary = yesNo();
+
+            var isOrNot;
+
+            if (binary == "Yes" && subject == "I") {
+                isOrNot = "am";
+            }
+
+            else if (binary == "No" && subject == "I") {
+                isOrNot = "am not";
+            }
+                
+            else if (binary == "Yes" && subject == "you") {
+                isOrNot = "are";
+            }
+
+            else if (binary == "No" && subject == "you") {
+                isOrNot = "are not";
+            }
+
+            else if (binary == "Yes") {
+                isOrNot = "is";
+            }
+
+            else {
+                isOrNot = "is not";
+            }
+            
+            output = `${binary}, ${subject} ${isOrNot} ${object}`;
+        }
+
+        else if (input.match(/^are (\w+) (\w+)(\?)?$/i)) {
+            var [, subject, object] = input.match(/^are (\w+) (\w+)(\?)?$/i);
+            var [, subjectCapitalised, objectCapitalised] = inputUnsanitised.match(/^are (\w+) (\w+)(\?)?$/i);
+
+            if (subject != subjectCapitalised) {
+                subject = subjectCapitalised;
+            }
+
+            if (object != objectCapitalised) {
+                object = objectCapitalised;
+            }
+
+            const binary = yesNo();
+
+            var isOrNot;
+
+            if (binary == "Yes" && subject == "I") {
+                isOrNot = "am";
+            }
+
+            else if (binary == "No" && subject == "I") {
+                isOrNot = "am not";
+            }
+                
+            else if (binary == "Yes" && subject == "you") {
+                isOrNot = "are";
+            }
+
+            else if (binary == "No" && subject == "you") {
+                isOrNot = "are not";
+            }
+
+            else if (binary == "Yes") {
+                isOrNot = "is";
+            }
+
+            else {
+                isOrNot = "is not";
+            }
+            
+            output = `${binary}, ${subject} ${isOrNot} ${object}`;
+        }
+
+        document.getElementById("output").innerText = output;
+
+        document.getElementById("output").style.opacity = "1";
+    }, 250);
+}
+
+function yesNo() {
+    if (Math.random() > 0.5) {
+        return "Yes";
+    }
+
+    else {
+        return "No";
+    }
+}
+
+function swapPronouns(sentence) {
+    // Replace "you" with "I" and vice versa using regular expressions
+    const swappedSentence = sentence
+        .replace(/\bYou\b/gi, '###TEMP###')
+        .replace(/\bI\b/gi, 'you')
+        .replace(/###TEMP###/gi, 'I');
+
+    return swappedSentence;
+}
